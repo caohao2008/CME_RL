1348fe36972c025410b63e87e1e5d0b875cb8f39
caohao <caohao@caohaodeMacBook-Pro.local>
2018-07-20 11:36:25 +0800

    improve

--- cem_rl.py
+++ cem_rl.py
@@ -19,6 +19,7 @@ def check_state(state,action_list,min_test_time):
             result = 0
     return result
 
+#update trans probability according to the past test
 def update_probability_topn(state,action_list,topn):
     topn_sum = 0
     current_state_dis={}
@@ -36,7 +37,7 @@ def update_probability_topn(state,action_list,topn):
     for sa,value in sorted_actions:
         cur_n+=1
         if cur_n<=topn:
-            print sa,value,cur_n
+            print sa,samap[sa],value,cur_n
             topn_sum+=value
         else:
             break
@@ -58,7 +59,7 @@ samap_average={}
 probability_trans={}
 action_list=set()
 state_set=set()
-for line in open("part-00000"):
+for line in sys.stdin:
     cols = line.strip().split(";")
     if len(cols)<5:
         continue
@@ -88,15 +89,19 @@ for line in open("part-00000"):
     #print cols
 print samap
 print samap_average
+
+fully_tested_state_cnt=0
 for state in state_set:
-    print state
+    ##print state
     #check whether current state is exploid fully: 1.All the action has been tested 2.Each action satisfy minimal test time
     if check_state(state,action_list,1):
         #if explore fully, update probability
-        print_state_action_detail(state,action_list)
+        #print_state_action_detail(state,action_list)
         update_probability_topn(state,action_list,4)
+        fully_tested_state_cnt+=1
     ##else:
         #print reason for check_state failed
     ##    print_state_action_detail(state,action_list)
 
 print probability_trans
+print "total state cnt = ",str(len(state_set))," , fully tested cnt = ",str(fully_tested_state_cnt)
